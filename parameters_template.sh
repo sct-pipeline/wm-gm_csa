d65a80b87adadbe8af4da0fa6bb5de91e516f3f3
François Paugam <francois.paugam@laposte.net>
2018-10-02 22:37:43 +0200

    update parameters template

--- parameters_template.sh
+++ parameters_template.sh
@@ -5,31 +5,22 @@
 
 # Path to working directory, which contains data results, qc, etc. (do not add "/" at the end).
 # This path should be absolute (i.e. should start with "/"). Example: PATH_DATA="/Users/bob/experiments"
-export PATH_MAIN="/Users/julien/data/nih_seg"
+export PATH_MAIN="/Users/francois/Documents/Neuropoly/results_csa"
 
 # Path to the folder which contains all data
-export PATH_DATA="/Users/julien/Dropbox/marmoset_SC_forCharley"
+export PATH_DATA="/Users/francois/Documents/Neuropoly/marmoset_data"
 
 # List of subjects to analyse. If you want to analyze all subjects in the
 # PATH_DATA folder, then comment this variable.
 export SUBJECTS=(
-	"Eva"
-	"Gretel"
-	"luisa"
+	#"Eva"
+	"gretel"
+	#"luisa"
 	)
 
-export FILE_WM="t2s_cerv_nawm_seg.nii.gz"
+# tag used to identify WM files, it must be included in all and only in WM file names
+export TAG_FILE_WM="nawm_seg"
+# tag used to identify GM files, it must be included in all and only in GM file names
+export TAG_FILE_GM="nagm_seg"
 
-# vertebral or disc level where FOV is centered. Uncomment the appropriate
-# variable and assign value:
-# export MIDFOV_DISC=X
-# export MIDFOV_VERT=X
 
-# Vertebral levels to compute MRI metrics from
-# export METRICS_VERT_LEVEL="3,4,5"
-
-# Path where to save results (do not add "/" at the end).
-# export PATH_RESULTS="${PATH_MAIN}/results"
-
-# Path where to save QC results (do not add "/" at the end).
-# export PATH_QC="${PATH_MAIN}/qc"
